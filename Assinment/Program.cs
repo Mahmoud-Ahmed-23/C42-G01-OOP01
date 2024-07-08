3432abc4153a853908b58929436c9d06dd52e2e5
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-07-08 14:23:21 +0300

    Question 5

--- Assinment/Program.cs
+++ Assinment/Program.cs
@@ -101,141 +101,154 @@
 
             #region Question 4
 
-            Permissions permission = new Permissions();
+            //Permissions permission = new Permissions();
 
-            while (true)
-            {
-                Console.Clear();
-                Console.Write("Enter (1) if you want to add Permissions\nEnter (2) if you want to remove Permissions\nEnter (3) if you want to check Exist Permissions\nEnter (another thing) to Exit the program: ");
+            //while (true)
+            //{
+            //    Console.Clear();
+            //    Console.Write("Enter (1) if you want to add Permissions\nEnter (2) if you want to remove Permissions\nEnter (3) if you want to check Exist Permissions\nEnter (another thing) to Exit the program: ");
 
-                int check = int.Parse(Console.ReadLine() ?? "0");
-
-
-                if (check == 1)
-                {
-                    while (true)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("1. Enter (1 or Read) to add Read Permission");
-                        Console.WriteLine("2. Enter (2 or Write) to add Write Permission");
-                        Console.WriteLine("3. Enter (3 or Delete) to add Delete Permission");
-                        Console.WriteLine("4. Enter (4 or Execute) to add Execute Permission");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will added (Enter 3 will add 'Read and Write')");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        string word = Console.ReadLine() ?? "0";
-
-                        int number;
-
-                        if (int.TryParse(word, out number) && number > 0)
-                            permission |= (Permissions)number;
-                        else if (number == -1)
-                            break;
-                        else
-                            permission |= Enum.Parse<Permissions>(word, true);
-
-                    }
-
-                }
-                else if (check == 2)
-                {
-                    while (true)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("1. Enter (1 or Read) to Remove Read Permission");
-                        Console.WriteLine("2. Enter (2 or Write) to Remove Write Permission");
-                        Console.WriteLine("3. Enter (3 or Delete) to Remove Delete Permission");
-                        Console.WriteLine("4. Enter (4 or Execute) to Remove Execute Permission");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will Removed (Enter 3 will Remove 'Read and Write')");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        string word = Console.ReadLine() ?? "0";
-
-                        int number;
-
-                        if (int.TryParse(word, out number) && number > 0)
-                            permission -= (Permissions)number;
-                        else if (number == -1)
-                            break;
-                        else
-                            permission -= Enum.Parse<Permissions>(word, true);
-
-                    }
-                }
-                else if (check == 3)
-                {
-
-                    Console.Clear();
-                    while (true)
-                    {
-
-                        Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("1. Enter (1 or Read) to Check if Read Permission is Exist");
-                        Console.WriteLine("2. Enter (2 or Write) to Check if Write Permission is Exist");
-                        Console.WriteLine("3. Enter (3 or Delete) to Check if Delete Permission is Exist");
-                        Console.WriteLine("4. Enter (4 or Execute) to Check if Execute Permission is Exist");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will Checked (Enter 3 will Check if 'Read and Write' are Exist)");
-
-                        Console.WriteLine("\n-------------------------------------------\n");
-
-                        string word = Console.ReadLine() ?? "0";
-                        int number;
-                        Console.Clear();
+            //    int check = int.Parse(Console.ReadLine() ?? "0");
 
 
-                        if (int.TryParse(word, out number) && number > 0)
-                        {
-                            if (permission.HasFlag((Permissions)number))
-                                Console.WriteLine($"Yes permission {(Permissions)number} is Exists");
-                            else
-                                Console.WriteLine($"No permission {(Permissions)number} isn't Exists");
-                        }
-                        else if (number == -1)
-                            break;
-                        else
-                        {
-                            if (permission.HasFlag(Enum.Parse<Permissions>(word, true)))
-                                Console.WriteLine($"Yes permission {Enum.Parse<Permissions>(word, true)} is Exists");
-                            else
-                                Console.WriteLine($"No permission {Enum.Parse<Permissions>(word, true)} isn't Exists");
-                        }
-                        Console.WriteLine("\n");
-                    }
+            //    if (check == 1)
+            //    {
+            //        while (true)
+            //        {
+            //            Console.Clear();
+
+            //            Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("1. Enter (1 or Read) to add Read Permission");
+            //            Console.WriteLine("2. Enter (2 or Write) to add Write Permission");
+            //            Console.WriteLine("3. Enter (3 or Delete) to add Delete Permission");
+            //            Console.WriteLine("4. Enter (4 or Execute) to add Execute Permission");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will added (Enter 3 will add 'Read and Write')");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            string word = Console.ReadLine() ?? "0";
+
+            //            int number;
+
+            //            if (int.TryParse(word, out number) && number > 0)
+            //                permission |= (Permissions)number;
+            //            else if (number == -1)
+            //                break;
+            //            else
+            //                permission |= Enum.Parse<Permissions>(word, true);
+
+            //        }
+
+            //    }
+            //    else if (check == 2)
+            //    {
+            //        while (true)
+            //        {
+            //            Console.Clear();
+
+            //            Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("1. Enter (1 or Read) to Remove Read Permission");
+            //            Console.WriteLine("2. Enter (2 or Write) to Remove Write Permission");
+            //            Console.WriteLine("3. Enter (3 or Delete) to Remove Delete Permission");
+            //            Console.WriteLine("4. Enter (4 or Execute) to Remove Execute Permission");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will Removed (Enter 3 will Remove 'Read and Write')");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            string word = Console.ReadLine() ?? "0";
+
+            //            int number;
+
+            //            if (int.TryParse(word, out number) && number > 0)
+            //                permission -= (Permissions)number;
+            //            else if (number == -1)
+            //                break;
+            //            else
+            //                permission -= Enum.Parse<Permissions>(word, true);
+
+            //        }
+            //    }
+            //    else if (check == 3)
+            //    {
+
+            //        Console.Clear();
+            //        while (true)
+            //        {
+
+            //            Console.WriteLine("Please Enter the Permission or Enter -1 to Exit the Program");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("1. Enter (1 or Read) to Check if Read Permission is Exist");
+            //            Console.WriteLine("2. Enter (2 or Write) to Check if Write Permission is Exist");
+            //            Console.WriteLine("3. Enter (3 or Delete) to Check if Delete Permission is Exist");
+            //            Console.WriteLine("4. Enter (4 or Execute) to Check if Execute Permission is Exist");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            Console.WriteLine("# (important)\n Entering number equal summition of two Permissions the two Permissions will Checked (Enter 3 will Check if 'Read and Write' are Exist)");
+
+            //            Console.WriteLine("\n-------------------------------------------\n");
+
+            //            string word = Console.ReadLine() ?? "0";
+            //            int number;
+            //            Console.Clear();
 
 
-                }
-                else
-                { break; }
-            }
+            //            if (int.TryParse(word, out number) && number > 0)
+            //            {
+            //                if (permission.HasFlag((Permissions)number))
+            //                    Console.WriteLine($"Yes permission {(Permissions)number} is Exists");
+            //                else
+            //                    Console.WriteLine($"No permission {(Permissions)number} isn't Exists");
+            //            }
+            //            else if (number == -1)
+            //                break;
+            //            else
+            //            {
+            //                if (permission.HasFlag(Enum.Parse<Permissions>(word, true)))
+            //                    Console.WriteLine($"Yes permission {Enum.Parse<Permissions>(word, true)} is Exists");
+            //                else
+            //                    Console.WriteLine($"No permission {Enum.Parse<Permissions>(word, true)} isn't Exists");
+            //            }
+            //            Console.WriteLine("\n");
+            //        }
+
+
+            //    }
+            //    else
+            //    { break; }
+            //}
 
             #endregion
 
 
+            #region Question 5
 
+            Console.Write("Enter a Color: ");
+
+            Colors color = new Colors();
+
+            string input = Console.ReadLine() ?? "";
+
+            if (Enum.TryParse(input, true, out color))
+                Console.WriteLine($"{input} is Primary Color");
+            else
+                Console.WriteLine($"{input} is not Primary Color");
+
+            #endregion
 
 
 
